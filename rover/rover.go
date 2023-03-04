ca82b8d6993a09f376d6273ed3f4ef9eb1a77ca4
Guilherme Paixão <guiferpa@gmail.com>
2023-03-04 00:21:42 -0300

    feat: created logic to rover move on

--- rover/rover.go
+++ rover/rover.go
@@ -4,6 +4,8 @@ import "strings"
 
 type Rover struct {
 	dir int
+	x   int
+	y   int
 }
 
 func (r *Rover) TurnLeft() {
@@ -22,6 +24,28 @@ func (r *Rover) TurnRight() {
 	}
 
 	r.dir -= 1
+}
+
+func (r *Rover) MoveOn() {
+	if r.dir == North {
+		r.y += 1
+		return
+	}
+
+	if r.dir == West {
+		r.x -= 1
+		return
+	}
+
+	if r.dir == South {
+		r.y -= 1
+		return
+	}
+
+	if r.dir == East {
+		r.x += 1
+		return
+	}
 }
 
 func (r *Rover) Walk(inst string) error {
