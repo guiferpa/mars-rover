888bc7e9eede5fb5ec6681b6b61d11d5ceac6830
Guilherme Paixão <guiferpa@gmail.com>
2023-03-05 12:25:36 -0300

    test: improved test for file args

--- cmd/cli/main_test.go
+++ cmd/cli/main_test.go
@@ -1,16 +1,16 @@
-//go:build integration
-
 package main
 
 import (
 	"bytes"
 	"io"
+	"io/ioutil"
+	"os"
 	"os/exec"
 	"strings"
 	"testing"
 )
 
-func TestIntegrationCLI(t *testing.T) {
+func TestIntegrationCLIByStdin(t *testing.T) {
 	value := `5 5
 1 2 N
 LMLMLMLMM
@@ -35,6 +35,46 @@ MMRMMRMRRM
 
 		io.WriteString(stdin, value)
 	}()
+
+	output, err := sub.CombinedOutput()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	buf := bytes.NewBuffer(output)
+	if got, expected := buf.String(), expected; strings.Compare(got, expected) != 0 {
+		t.Errorf("unexpected result for integration test, got: %s, expected: %s", got, expected)
+		return
+	}
+}
+
+func TestIntegrationCLIByFileArgs(t *testing.T) {
+	value := `5 5
+1 2 N
+LMLMLMLMM
+3 3 E
+MMRMMRMRRM
+`
+
+	expected := `1 3 N
+5 1 E
+`
+
+	f, err := ioutil.TempFile("/tmp", "mars-rover-*.txt")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer os.Remove(f.Name())
+
+	f.WriteString(value)
+	if err := f.Sync(); err != nil {
+		t.Error(err)
+		return
+	}
+
+	sub := exec.Command("go", "run", "./main.go", "--file", f.Name())
 
 	output, err := sub.CombinedOutput()
 	if err != nil {
