e05fe7017900b98a1f12309fa4e72d042cdd00de
Guilherme Paixão <guiferpa@gmail.com>
2023-03-05 12:25:13 -0300

    feat: added support to file args

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,31 +2,62 @@ package main
 
 import (
 	"bufio"
+	"flag"
+	"log"
+	"os"
+
 	"github/guiferpa/mars-rover/domain/rover"
 	"github/guiferpa/mars-rover/handler/interface/cli"
 	roversdk "github/guiferpa/mars-rover/infra/sdk/rover"
-	"os"
 )
 
-func buildInput() []string {
+var file string
+
+func init() {
+	flag.StringVar(&file, "file", "", "file input")
+}
+
+func buildInput() ([]string, error) {
 	input := make([]string, 0)
+
+	if file != "" {
+		f, err := os.Open(file)
+		if err != nil {
+			return []string{}, err
+		}
+
+		scanner := bufio.NewScanner(f)
+		for scanner.Scan() {
+			text := scanner.Text()
+			input = append(input, text)
+		}
+
+		return input, nil
+	}
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		text := scanner.Text()
 		input = append(input, text)
 	}
 
-	return input
+	return input, nil
 }
 
 func main() {
+	flag.Parse()
+
 	sdk := roversdk.NewRoverSDK()
 	service := rover.NewUseCaseService(sdk)
 	cli := cli.NewInterface(service)
 
-	input := buildInput()
+	input, err := buildInput()
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
 	if err := cli.Run(input); err != nil {
-		panic(err)
+		log.Println(err)
 	}
 }
