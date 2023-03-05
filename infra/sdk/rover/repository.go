05c6038a605c749dd934f9fc9dc9094242232d9f
Guilherme Paixão <guiferpa@gmail.com>
2023-03-05 00:12:56 -0300

    feat: created infra layer for implements rover SDK
    This Rover SDK is a helper just for handle data structure
    about rover coords and direction. This helps in clean the
    source code and reusable as thrid lib for another project

---
+++ infra/sdk/rover/repository.go
@@ -0,0 +1,29 @@
+package rover
+
+import roverpkg "github/guiferpa/foxbit-challenge/pkg/rover"
+
+type RoverSDK struct {
+	rover *roverpkg.Rover
+}
+
+func (rs *RoverSDK) SetPosition(x, y int, dir string) error {
+	return rs.rover.SetPosition(x, y, dir)
+}
+
+func (rs *RoverSDK) TurnLeft() {
+	rs.rover.TurnLeft()
+}
+
+func (rs *RoverSDK) TurnRight() {
+	rs.rover.TurnRight()
+}
+
+func (rs *RoverSDK) GetPosition() string {
+	return rs.rover.GetPosition()
+}
+
+func NewRoverSDK() *RoverSDK {
+	return &RoverSDK{
+		rover: &roverpkg.Rover{},
+	}
+}
